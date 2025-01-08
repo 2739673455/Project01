2d6793653bb85bb77867f81769ab04f8abcd9718
lenovo_616 <2739673455@qq.com>
2025-01-08 17:42:06 +0800

    0

--- demo.py
+++ demo.py
@@ -1,13 +1,2 @@
 import numpy as np
 import pandas as pd
-
-
-def f(x, y):
-    if y == 0:
-        return np.nan
-    return x / y
-
-
-df = pd.DataFrame({"a": [10, 20, 30], "b": [40, 0, 60]})
-f_vec = np.vectorize(f)
-print(f_vec(df["a"], df["b"]))  # [0.25  nan 0.5 ]
