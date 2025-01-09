65ec0da6e432926a3d821f6864df7d7a4b2cfefd
lenovo_616 <2739673455@qq.com>
2025-01-09 18:02:30 +0800

    0

--- demo.py
+++ demo.py
@@ -1,5 +1,14 @@
+import numpy as np
 import pandas as pd
 
-df = pd.read_csv("data/weather.csv")
-df["date"] = pd.to_datetime(df["date"])
-print(df.info())
+df = pd.read_csv("data/employees.csv")  # 读取员工数据
+
+
+def f(x):
+    if x["job_id"] == "IT_PROG":
+        return 1.1 * x["salary"]
+    else:
+        return x["salary"]
+
+
+print(df["salary"] * df["commission_pct"] + df["salary"])
