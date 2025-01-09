2d0e55d1474ca715a9b34a3385f5ce730f6678d0
616abf12b201 <k2739673455@gmail.com>
2025-01-09 23:11:00 +0800

    0

--- demo.py
+++ demo.py
@@ -1,14 +1,33 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import rcParams
 
-df = pd.read_csv("data/employees.csv")  # 读取员工数据
-
-
-def f(x):
-    if x["job_id"] == "IT_PROG":
-        return 1.1 * x["salary"]
-    else:
-        return x["salary"]
+rcParams["font.sans-serif"] = ["SimHei"]  # 指定中文字体
+rcParams["axes.unicode_minus"] = False  # 解决负号显示问题
 
 
-print(df["salary"] * df["commission_pct"] + df["salary"])
+def year_color(x):
+    """添加一列，为不同年份的数据添加不同的颜色"""
+    match x.year:
+        case 2012:
+            return "r"
+        case 2013:
+            return "g"
+        case 2014:
+            return "b"
+        case 2015:
+            return "k"
+
+
+df = pd.read_csv("data/weather.csv")
+df["date"] = pd.to_datetime(df["date"])
+df["color"] = df["date"].apply(year_color)
+fig = plt.figure()
+ax1 = fig.add_subplot(1, 1, 1)
+# 绘制散点图，横轴为最高气温，纵轴为降水量
+# c设置颜色,alpha设置透明度
+ax1.scatter(df["temp_max"], df["precipitation"], c=df["color"], alpha=0.5)
+ax1.set_xlabel("最高气温")
+ax1.set_ylabel("降水量")
+plt.show()
