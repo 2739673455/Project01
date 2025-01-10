e14426fc3030cc72cf9cc89e8f964955c3972ab0
lenovo_616 <2739673455@qq.com>
2025-01-10 18:00:25 +0800

    0

--- demo.py
+++ demo.py
@@ -1,33 +1,14 @@
-import numpy as np
 import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
-from matplotlib import rcParams
 
-rcParams["font.sans-serif"] = ["SimHei"]  # 指定中文字体
-rcParams["axes.unicode_minus"] = False  # 解决负号显示问题
+plt.rcParams["font.sans-serif"] = ["SimHei"]
 
-
-def year_color(x):
-    """添加一列，为不同年份的数据添加不同的颜色"""
-    match x.year:
-        case 2012:
-            return "r"
-        case 2013:
-            return "g"
-        case 2014:
-            return "b"
-        case 2015:
-            return "k"
-
-
-df = pd.read_csv("data/weather.csv")
-df["date"] = pd.to_datetime(df["date"])
-df["color"] = df["date"].apply(year_color)
-fig = plt.figure()
-ax1 = fig.add_subplot(1, 1, 1)
-# 绘制散点图，横轴为最高气温，纵轴为降水量
-# c设置颜色,alpha设置透明度
-ax1.scatter(df["temp_max"], df["precipitation"], c=df["color"], alpha=0.5)
-ax1.set_xlabel("最高气温")
-ax1.set_ylabel("降水量")
+df = sns.load_dataset("tips")  # 加载内置的 tips 数据集
+fig, ax = plt.subplots()
+sns.histplot(data=df, x="total_bill", ax=ax)
+ax.set_title("总账单金额")
+plt.show()
+sns.kdeplot(data=df, x="total_bill", ax=ax)
+ax.set_title("总账单金额核密度估计图")
 plt.show()
