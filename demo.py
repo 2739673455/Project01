e8394c2703d85465472a9df9d2e6fb783322d437
616abf12b201 <k2739673455@gmail.com>
2025-01-11 19:47:02 +0800

    0

--- demo.py
+++ demo.py
@@ -1,14 +1,3 @@
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
 
-plt.rcParams["font.sans-serif"] = ["SimHei"]
-
-df = sns.load_dataset("tips")  # 加载内置的 tips 数据集
-fig, ax = plt.subplots()
-sns.histplot(data=df, x="total_bill", ax=ax)
-ax.set_title("总账单金额")
-plt.show()
-sns.kdeplot(data=df, x="total_bill", ax=ax)
-ax.set_title("总账单金额核密度估计图")
-plt.show()
+print(pd.__version__)
