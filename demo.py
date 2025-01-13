b0ba6ed40935730eae6657ea7cbb1c94fefcf76c
lenovo_616 <2739673455@qq.com>
2025-01-13 17:54:49 +0800

    0

--- demo.py
+++ demo.py
@@ -1,8 +1,3 @@
-import time
 import numpy as np
-import pandas as pd
-import pymysql
-from pyecharts.charts import Bar3D
 
-sheet_names = ["2015", "2016", "2017", "2018", "会员等级"]
-sheet_datas = pd.read_excel("data/sales.xlsx", sheet_name=sheet_names)
+print(np.random.rand(10, 10))
