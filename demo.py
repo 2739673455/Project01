6c5a940da13ce7437fc30e19655d4f15543c8752
lenovo_616 <2739673455@qq.com>
2025-01-13 11:46:54 +0800

    0

--- demo.py
+++ demo.py
@@ -1,3 +1,8 @@
+import time
+import numpy as np
 import pandas as pd
+import pymysql
+from pyecharts.charts import Bar3D
 
-print(pd.__version__)
+sheet_names = ["2015", "2016", "2017", "2018", "会员等级"]
+sheet_datas = pd.read_excel("data/sales.xlsx", sheet_name=sheet_names)
