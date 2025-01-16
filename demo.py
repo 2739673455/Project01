8831e022e1289a5bf30861911463af913f70b782
lenovo_616 <2739673455@qq.com>
2025-01-16 17:41:38 +0800

    0

--- demo.py
+++ demo.py
@@ -1,3 +1,24 @@
-import numpy as np
+import requests
+from openai import OpenAI
 
-print(np.random.rand(10, 10))
+resp = requests.post(
+    url="https://api.chatanywhere.tech/v1/chat/completions",
+    headers={
+        "Authorization": "Bearer sk-XTFNbTWwvJrOeAUekxS2BpP3MtGVmPEO026E0GxMPV8qPzxf",
+        "Content-Type": "application/json",
+    },
+    data="""
+    {
+        "model": "gpt-4o-mini",
+        "messages": [
+        {
+            "role": "system",
+            "content": "You"
+        },
+        {
+            "role": "user",
+            "content": "介绍一下你自己"
+        }]
+    }""",
+)
+print(resp.text)
