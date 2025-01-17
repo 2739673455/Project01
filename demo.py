809143b7615059577c849988fd51c0a12e7de9ee
lenovo_616 <2739673455@qq.com>
2025-01-17 17:55:06 +0800

    0

--- demo.py
+++ demo.py
@@ -1,24 +1,13 @@
-import requests
-from openai import OpenAI
+import numpy as np
 
-resp = requests.post(
-    url="https://api.chatanywhere.tech/v1/chat/completions",
-    headers={
-        "Authorization": "Bearer sk-XTFNbTWwvJrOeAUekxS2BpP3MtGVmPEO026E0GxMPV8qPzxf",
-        "Content-Type": "application/json",
-    },
-    data="""
-    {
-        "model": "gpt-4o-mini",
-        "messages": [
-        {
-            "role": "system",
-            "content": "You"
-        },
-        {
-            "role": "user",
-            "content": "介绍一下你自己"
-        }]
-    }""",
-)
-print(resp.text)
+np.set_printoptions(linewidth=150)
+x = np.random.randint(0, 10, (10, 5))  # 5个样本,10个特征
+x = np.vstack((np.ones(shape=(1, x.shape[1])), x))  # 添加一行1用作与偏置运算
+y = np.random.randint(0, 10, (1, 5))  # 5个标签
+k = np.random.rand(1, 11)  # 1个偏置，10个权重
+learn_rate = 0.01  # 学习率
+delta_k = np.eye(11) * 0.001
+epsilon = y - (k @ x)  # 残差
+epsilon2 = y - ((k - delta_k) @ x)
+print(epsilon2 - epsilon)  # 残差的变化
+print(k - delta_k)  # 权重的变化
