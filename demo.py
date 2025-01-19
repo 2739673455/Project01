030ef17f91dbc6f33606f374c106cadb2997ef47
616abf12b201 <k2739673455@gmail.com>
2025-01-19 21:50:50 +0800

    0

--- demo.py
+++ demo.py
@@ -1,34 +1,42 @@
 import numpy as np
 import time
 
+
+def func1(k1):
+    loss = y - (k1 @ x)
+    loss = np.sum(loss**2)
+    if loss < 1e-1:
+        return k1, loss
+    loss2 = y - ((k1 - np.eye(11) * delta_k) @ x)
+    loss2 = np.sum(loss2**2, axis=1)
+    k1 -= learn_rate * (loss - loss2) / delta_k
+    return k1, loss
+
+
+def func2(k2):
+    loss = k2 @ x - y
+    if np.sum(loss**2) < 1e-1:
+        return k2, loss
+    gradient = loss @ x.T
+    k2 -= learn_rate * gradient
+    return k2, loss
+
+
 np.set_printoptions(linewidth=150)
-x = np.random.randint(0, 10, (10, 5))  # 5个样本,10个特征
+x = np.random.randint(0, 10, (10, 500))  # 5个样本,10个特征
 x = np.vstack((np.ones(shape=(1, x.shape[1])), x))  # 添加一行1用来和偏置运算
-y = np.random.randint(0, 10, (1, 5))  # 5个标签
+y = np.random.randint(0, 10, (1, 500))  # 5个标签
 k = np.random.rand(1, 11)  # 1个偏置，10个权重
-learn_rate = 1e-4  # 学习率
-delta_k = np.eye(11) * 1e-5
+learn_rate = 1e-6  # 学习率
+delta_k = 1e-8
 
 epoch = 0
 k1 = k.copy()
-while True:
-    epsilon = y - (k1 @ x)
-    epsilon = np.sum(epsilon**2)
-    if epsilon < 1e-5:
-        break
-    epsilon2 = y - ((k1 - delta_k) @ x)
-    epsilon2 = np.sum(epsilon2**2, axis=1)
-    k1 += learn_rate * (epsilon2 - epsilon) / 1e-5
-    epoch += 1
-print(epsilon, epoch)
-
-epoch = 0
 k2 = k.copy()
 while True:
-    loss = k2 @ x - y
-    if np.sum(loss**2) < 1e-5:
+    k1, loss1 = func1(k1)
+    k2, loss2 = func2(k2)
+    if loss1 < 1e-1 and loss2 < 1e-1:
         break
-    gradient = loss @ x.T / 5
-    k2 -= learn_rate * gradient
     epoch += 1
-print(np.sum(loss**2), epoch)
+    print(loss1, "\t", np.sum(loss2**2), "\t", epoch)
