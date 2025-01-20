a274da1d53f1674cf51ae6a922e3c0053c3b5396
lenovo_616 <2739673455@qq.com>
2025-01-20 17:39:16 +0800

    0

--- demo.py
+++ demo.py
@@ -1,5 +1,6 @@
 import numpy as np
-import time
+
+np.set_printoptions(linewidth=500)
 
 
 def func1(k1):
@@ -7,9 +8,10 @@ def func1(k1):
     loss = np.sum(loss**2)
     if loss < 1e-1:
         return k1, loss
-    loss2 = y - ((k1 - np.eye(11) * delta_k) @ x)
+    loss2 = y - ((k1 + np.eye(11) * delta_k) @ x)
     loss2 = np.sum(loss2**2, axis=1)
-    k1 -= learn_rate * (loss - loss2) / delta_k
+    gradient = (loss2 - loss) / (delta_k * 2)
+    k1 -= learn_rate * gradient
     return k1, loss
 
 
@@ -22,21 +24,21 @@ def func2(k2):
     return k2, loss
 
 
-np.set_printoptions(linewidth=150)
-x = np.random.randint(0, 10, (10, 500))  # 5个样本,10个特征
+sample_count = 5
+x = np.random.randint(0, 10, (10, sample_count))  # 5个样本,10个特征
 x = np.vstack((np.ones(shape=(1, x.shape[1])), x))  # 添加一行1用来和偏置运算
-y = np.random.randint(0, 10, (1, 500))  # 5个标签
+y = np.random.randint(0, 10, (1, sample_count))  # 5个标签
 k = np.random.rand(1, 11)  # 1个偏置，10个权重
-learn_rate = 1e-6  # 学习率
+learn_rate = 1e-5  # 学习率
 delta_k = 1e-8
-
 epoch = 0
 k1 = k.copy()
 k2 = k.copy()
 while True:
     k1, loss1 = func1(k1)
     k2, loss2 = func2(k2)
-    if loss1 < 1e-1 and loss2 < 1e-1:
+    if loss1 < 1e-3 and loss2 < 1e-3:
         break
     epoch += 1
-    print(loss1, "\t", np.sum(loss2**2), "\t", epoch)
+    if epoch % 1000 == 0:
+        print(loss1, "\t", np.sum(loss2**2), "\t", epoch)
